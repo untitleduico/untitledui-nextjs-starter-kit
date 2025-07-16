de6605236971602b7e8951e576063c48b4102d2b
Dilshod <deebov@yandex.com>
2025-07-16 18:52:46 +0200

    Fix tooltip trigger (#2)

--- src/components/base/tooltip/tooltip.tsx
+++ src/components/base/tooltip/tooltip.tsx
@@ -146,16 +146,16 @@ type TooltipTriggerProps =
       };
 
 export const TooltipTrigger = (props: TooltipTriggerProps) => {
-    const triggerRef = useRef<FocusableElement>(null);
-
-    const { focusableProps } = useFocusable(
-        {
-            isDisabled: props.isDisabled,
-        },
-        triggerRef,
-    );
-
     if (props.asChild) {
+        const triggerRef = useRef<FocusableElement>(null);
+
+        const { focusableProps } = useFocusable(
+            {
+                isDisabled: props.isDisabled,
+            },
+            triggerRef,
+        );
+
         return cloneElement(props.children, mergeProps(focusableProps, props.children.props, { ref: triggerRef }));
     }
 
